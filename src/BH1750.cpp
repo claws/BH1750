fbc4981ee25ff73fde28713821b813cb8a1375f8
coelner <waenger@gmail.com>
2018-10-23 18:46:13 +0200

    fix begin() for MTreg default value

--- src/BH1750.cpp
+++ src/BH1750.cpp
@@ -75,6 +75,8 @@ bool BH1750::begin(Mode mode, byte addr, TwoWire *i2c) {
     BH1750_I2CADDR = addr;
   }
 
+  //return to a known value
+  setMTreg(BH1750_DEFAULT_MTREG);
   // Configure sensor in specified mode
   return configure(mode);
 
