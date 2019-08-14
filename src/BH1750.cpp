b8f7d70bc14f0283e4966d0552fd7a4fba6b6c15
coelner <waenger@googlemail.com>
2019-08-14 19:37:18 +0200

    fix continuous mode reset

--- src/BH1750.cpp
+++ src/BH1750.cpp
@@ -229,11 +229,6 @@ float BH1750::readLightLevel(bool maxWait) {
   // Measurement result will be stored here
   float level = -1.0;
 
-  // Send mode to sensor
-  I2C->beginTransmission(BH1750_I2CADDR);
-  __wire_write((uint8_t)BH1750_MODE);
-  I2C->endTransmission();
-
   // Wait for measurement to be taken.
   // Measurements have a maximum measurement time and a typical measurement
   // time. The maxWait argument determines which measurement wait time is
@@ -247,10 +242,18 @@ float BH1750::readLightLevel(bool maxWait) {
   switch (BH1750_MODE) {
 
     case BH1750::ONE_TIME_LOW_RES_MODE:
+      // Send mode to sensor
+      Wire.beginTransmission(BH1750_I2CADDR);
+      __wire_write((uint8_t)BH1750_MODE);
+      Wire.endTransmission();
       maxWait ? _delay_ms(24 * BH1750_MTreg/(byte)BH1750_DEFAULT_MTREG) : _delay_ms(16 * BH1750_MTreg/(byte)BH1750_DEFAULT_MTREG);
       break;
     case BH1750::ONE_TIME_HIGH_RES_MODE:
     case BH1750::ONE_TIME_HIGH_RES_MODE_2:
+      // Send mode to sensor
+      Wire.beginTransmission(BH1750_I2CADDR);
+      __wire_write((uint8_t)BH1750_MODE);
+      Wire.endTransmission();
       maxWait ? _delay_ms(180 * BH1750_MTreg/(byte)BH1750_DEFAULT_MTREG) :_delay_ms(120 * BH1750_MTreg/(byte)BH1750_DEFAULT_MTREG);
       break;
     default:
